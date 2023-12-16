2c5fea0027e9c071f0ad1cb33cfc4e0b9346f2af
wlthdud <thdudzi@naver.com>
2023-12-16 23:45:59 +0900

    5. react-router-dom 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,6 +5,8 @@ import bg from './img/bg.png';
 
 import data from './data.js';
 
+import { Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
 
@@ -16,27 +18,53 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">NeXT Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-            <Nav.Link href="#pricing">MyPage</Nav.Link>
+            <Link to="/" style={{marginRight:"10px", textDecoration:"none", color:"grey"}}>홈</Link>
+            <Link to="/detail" style={{marginRight:"10px", textDecoration:"none", color:"grey"}}>상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
-      <Container>
-      <Row>
-        {
-          shoes.map((a,i)=>{
-            return(
-              <Card shoes={shoes[i]} i = {i+1}/>
-            )
-          })
-        }
-      </Row>
-    </Container>
-    </div>
+
+      <Routes>
+        <Route path="/" element={
+        <>
+          <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
+          <Container>
+          <Row>
+          {
+            shoes.map((a,i)=>{
+              return(
+                <Card shoes={shoes[i]} i = {i+1}/>
+              )
+            })}
+          </Row>
+          </Container>
+        </>
+        }/>
+        <Route path="/detail" element={<Detail/>}/>
+      </Routes>
+
+      
+  </div>
   );
+}
+
+function Detail(){
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+    </div> 
+  )
 }
 
 function Card(props){
