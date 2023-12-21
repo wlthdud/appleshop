5278b45644c392441a94e802eebcccf296f40c3d
wlthdud <thdudzi@naver.com>
2023-12-22 01:59:03 +0900

    9. Lifecycle과 useEffect 1

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,15 +1,16 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
-
-let YellowBtn = styled.button`
-  background : ${ props => props.bg }; 
-  color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
-  padding : 10px;
-`
-
 
 function Detail(props){
 
+  let [alert, setAlert] = useState(true);
+  useEffect(()=>{
+    setTimeout(() => { setAlert(false) }, 2000);
+  },[]);
+  
+  let [count, setCount] = useState(0);
+
+  
   let {id} = useParams();
   
   let findData = props.shoes.find(function(x){
@@ -18,7 +19,14 @@ function Detail(props){
   
     return(
       <div className="container">
-        <YellowBtn bg="blue">버튼</YellowBtn>
+        {
+          alert == true 
+          ? <div className="alert alert-warning">
+            2초이내 구매시 할인
+            </div> 
+            : null
+        }
+        <button onClick={()=>{setCount(count+1)}}>버튼</button>
         <div className="row">
           <div className="col-md-6">
             <img src={"https://codingapple1.github.io/shop/shoes" + (parseInt(id)+1) + ".jpg"} width="100%" />
